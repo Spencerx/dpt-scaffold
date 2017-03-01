008cd5b557991b80a19b36a60990ecef74fbee83
Artem Shitov <artemshitov@yandex-team.ru>
2017-03-01 21:17:23 +0300

    Init

--- index.js
+++ index.js
@@ -1,46 +1,34 @@
 var vfs = require('vinyl-fs');
 var through = require('through');
+var Handlebars = require('handlebars');
+var isTextOrBinary = require('istextorbinary');
 
-var fs = require('fs');
 var Path = require('path');
 
-function exists(path) {
-    return new Promise(function(resolve) {
-        return fs.exists(path, resolve);
-    });
-}
-
 module.exports = function(source, dest, config) {
-    var textuals = ['.js', '.less', '.svg', '.html', '.yaml', '.md'];
-
     return new Promise(function(resolve, reject) {
-        // Check if the destination already exists
-        return exists(dest)
-            .then(function(exists) {
-                if (exists) {
-                    return reject(new Error('Destination already exists'));
-                } else {
-                    var stream = vfs.src(Path.resolve(source, '**/*.*'), {
-                        allowEmpty: true
-                    });
+        var stream = vfs.src(Path.resolve(source, '**/*.*'), {
+            allowEmpty: true,
+            dot: true,
+            ignore: [Path.resolve(source, '.scaffold.json')]
+        });
 
-                    stream.pipe(through(function(_file) {
-                        var file = _file.clone();
-                        file.path = Handlebars.compile(file.path)(config);
+        stream.pipe(through(function(_file) {
+            var file = _file.clone();
+            file.path = Handlebars.compile(file.path)(config);
 
-                        // If file has textual contents
-                        if (textuals.includes(Path.extname(file.path))) {
-                            var source = file.contents.toString('utf-8');
-                            file.contents = new Buffer(Handlebars.compile(source)(config));
-                        }
+            // If file has textual contents
+            if (isTextOrBinary.isTextSync(file.path, file.contents)) {
+                var source = file.contents.toString(isTextOrBinary.getEncodingSync(file.contents));
+                file.contents = new Buffer(Handlebars.compile(source)(config));
+            }
+            return this.queue(file);
+        })).pipe(vfs.dest(dest, {
+            overwrite: false
+        }));
 
-                        return this.queue(file);
-                    })).pipe(vfs.dest(dest));
-
-                    stream.on('error', reject);
-                    stream.on('end', function() { resolve(config) });
-                    return stream;
-                }
-            });
+        stream.on('error', reject);
+        stream.on('end', function() { resolve(config) });
+        return stream;
     });
 }
